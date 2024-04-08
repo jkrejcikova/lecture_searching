d986fcdfb827aa1b6884cc0516a3c24b6de1b346
jkrejcikova <257017@vutbr.cz>
2024-04-08 12:17:19 +0200

    pokus

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -12,6 +13,15 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open ("sequential.json", mode="r") as data_file:
+        data = json.load(data_file)
+    for key in data.keys():
+        if field == key:
+            sequential_data = data[field]
+            return sequential_data
+        else:
+            return None
+
 
 
 def main():
